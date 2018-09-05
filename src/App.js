6ee8f7b53a6e18a59e96a12907b2cff540790d70
Khoi <hakhoipham@gmail.com>
2018-09-05 16:35:03 -0400

    initial setup of retrieving data from database done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,93 @@ import firebase from "./firebase";
 
 const dbRef = firebase.database().ref();
 
+/* ===================
+TEST STUFF BELOW THAT CAN BE DELETED WHEN DONE
+==================== */
+let user = "Jeff";
+let friends = [
+  {
+    name: "Dianna",
+    allergies: ["Candy", "Bananas"],
+    parties: ["sept3", "oct5"]
+  },
+  {
+    name: "Moin",
+    allergies: ["Grapes", "Cookies"],
+    parties: ["sept3", "oct5"]
+  }
+];
+
+let parties = [
+  {
+    title: "sept3",
+    recipes: ["Pie", "McDonald's"]
+  },
+  {
+    title: "oct5",
+    recipes: ["Peanut", "Pizza"]
+  }
+];
+
+/* ===================
+TEST STUFF ABOVE THAT CAN BE DELETED WHEN DONE
+==================== */
+
+// let key = dbRef.push(user).key;
+// dbRef.child(key).set({ user, friends, parties });
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userProfile: {}
+    };
+  }
+
+  /* FUNCTION TO GET EVENTS FROM FIREBASE */
+  retrieveEventsFromFirebase = snapshot => {
+    let newResults = Object.values(snapshot);
+
+    newResults.forEach(person => {
+      if (user === person.user) {
+        this.setState({
+          userProfile: newResults
+        });
+      }
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>JDK PROJECT!!!</h1>
+        {Object.values(this.state.userProfile).map(property => {
+          if (property.user === user) {
+            return (
+              <div>
+                <h1>{property.user}</h1>
+                {property.parties.map(party => {
+                  return (
+                    <div>
+                      <h2>{party.title}</h2>
+                      {party.recipes.map(recipe => {
+                        return <h3>{recipe}</h3>;
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }
+        })}
       </div>
     );
+  }
+
+  componentDidMount() {
+    dbRef.on("value", snapshot => {
+      this.retrieveEventsFromFirebase(snapshot.val());
+    });
   }
 }
 
