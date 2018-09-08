e544f5a58fa503ee22874b341199568ec49fc473
Daniel <dtkahn@gmail.com>
2018-09-08 14:23:50 -0400

    Starting initial styling, added wrapper class and added wrapper to overview

--- src/components/DisplayMatchingRecipes/DisplaySingleRecipe.js
+++ src/components/DisplayMatchingRecipes/DisplaySingleRecipe.js
@@ -5,18 +5,24 @@ import React from "react";
 const DisplaySingleRecipe = (props) => {
     return(
 
-        <li>
+        <li className="recipe-each">
+            
             <a href={`http://www.yummly.com/recipe/${props.recipe.id}`} target="_blank">
-                <h2>{props.recipe.recipeName}</h2>
-                <div className="img">
+                
+                <h2 className="recipe-name">{props.recipe.recipeName}</h2>
+                
+                <div className="recipe-img-container">
                     
                     {/* Removing the last 6 characters from the provided url '=s90-c' which appear to set the size of the image to a 90 pixel square */}
-                    <img src={
+                    <img className="recipe-img" src={
                         props.recipe.imageUrlsBySize['90'].substring(0, props.recipe.imageUrlsBySize['90'].length - 6)
-                        } alt={props.recipe.recipeName}/>
+                        } alt={props.recipe.recipeName}
+                    />
+
                 </div>
 
             </a>
+            
         </li>
     )
 }
