9096d732748451eff7a17933644eb2114026ad32
Daniel <dtkahn@gmail.com>
2018-09-11 21:34:41 -0400

    header rebuilt for event page desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,7 @@ const Header = (props) => {
                     <h1 className="app-name">DinnerSafe</h1>
                     <div className="user clearfix">
                         <h2>{props.user}</h2>
-                        {/* <div className="log-out-div clearfix"> */}
-                            <Link className="log-out" onClick={props.handleLogout} to="/">Log Out</Link>
-                        {/* </div> */}
+                        <Link className="log-out" onClick={props.handleLogout} to="/">Log Out</Link>
                     </div>
                 </div>
             </div>
